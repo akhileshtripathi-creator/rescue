00619cf07b68a27f5f8410b70932edd8ab0cdeb1
akhileshtripathi-creator <akhileshtiwari8576940121@gmail.com>
2025-09-12 22:21:35 +0530

    Update server.js
    Signed-off-by: akhileshtripathi-creator <akhileshtiwari8576940121@gmail.com>

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get("/rescue", async (req, res) => {
 
 // ✅ Start server
 app.listen(PORT, () => {
-  console.log(🚀 Server running at http://localhost:${PORT});
+  console.log( Server running at http://localhost:${PORT});
 });
